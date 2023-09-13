5418ea99d47ff5e26490d760f01ed1073bf74b38
ransiSe7en <ransihowler@gmail.com>
2023-09-13 22:45:43 +0530

    Aipickeredit

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,10 +1,8 @@
 import CustomButton from "./CustomButton";
-import AIPicker from "./AIpicker";
+import AIPicker from "./AIPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
 import Tab from "./Tab";
-
-
 
 export {
      CustomButton,
